fdc71f4c4ece2cbcdf152114bd13740ea6919bf6
tarnas14 <tarnas14@gmail.com>
2017-08-16 15:28:18 +0200

    removing everything from master for the next glitch

--- campaign.js
+++
@@ -1,1 +0,0 @@
-module.exports = {name:"voucherify-redemption-example", type: "AUTO_UPDATE", voucher: {type: "DISCOUNT_VOUCHER", discount: {"percent_off": 10, "type": "PERCENT"}}}
--- products.js
+++
@@ -1,3 +0,0 @@
-module.exports = [
-  { name: 'feynman', displayName: "📖 Surely you\'re joking, Mr. Feynman!", price: 10, id: "someVeryRandomId123" }
-]
