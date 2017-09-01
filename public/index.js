83ef8da26eec6982d26d01cb9a95f3afd79670ae
tarnas14 <tarnas14@gmail.com>
2017-09-01 08:53:40 +0200

    base html structure

--- public/index.js
+++ public/index.js
@@ -3,3 +3,17 @@ const documentReady = new Promise(resolve => document.addEventListener('DOMConte
 const socket = io()
 socket.on('connect', () => console.log('socket connected'))
 socket.on('disconnect', () => console.log('socket disconnected'))
+
+let state = {}
+
+const _addRedemptionEvent = (redemption) => {
+  
+}
+
+documentReady.then(() => {
+  document.getElementById('redeem').addEventListener('click', () => {
+    _addRedemptionEvent({
+    
+    })
+  })
+})
